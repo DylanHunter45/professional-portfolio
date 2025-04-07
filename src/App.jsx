65c0d7863d38bf1566e6a751c13722af38f010bd
Dylan Hunter <dylanhunter34@gmail.com>
2025-04-07 18:24:23 -0400

    add initial application structure with loading screen and styling

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { LoadingScreen } from "./components/LoadingScreen";
+import "./App.css";
+import "./index.css";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div>Portfolio section</div>
+    </>
+  );
+}
+
+export default App;
