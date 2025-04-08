98f57d5e0453811049c0b2a9a004f67eff2d2b07
Dylan Hunter <dylanhunter34@gmail.com>
2025-04-08 17:53:49 -0400

    feat: update Home component to display introduction section with gradient text

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+//from-blue-500 to-purple-500 bg-clip-text text-transparent <- for gradient text
 
 export class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <section
+        id="home"
+        className="min-h-screen flex items-center justify-center relative"
+      >
+        <div className="text-center x-10 px-4">
+          <h1 className="text-4xl md:text-7xl font-bold mb-6 bg-gradient-to-r  leading-right">
+            Hello, my name is Dylan Hunter.
+          </h1>
+        </div>
+      </section>
+    );
   }
 }
 
