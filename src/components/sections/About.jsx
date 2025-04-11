62683224dd9823104c514d8de63a4499d4def6ec
Dylan Hunter <dylanhunter34@gmail.com>
2025-04-11 14:56:28 -0400

    feat: add backend and frontend skills arrays in About component

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+const backendSkills = ["Python", "C# .NET"];
+const frontendSkills = ["Flutter", "React", "Vue", "JavaScript", "TailwindCSS"];
 
 export class About extends Component {
   render() {
