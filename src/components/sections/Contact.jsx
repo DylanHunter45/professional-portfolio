ed3d0fe0ac7126d7c0d835dcf6b9f30de95cc0e5
Dylan Hunter <dylanhunter34@gmail.com>
2025-04-11 16:27:47 -0400

    feat: add dotenv dependency and implement email submission in Contact component

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,59 +1,87 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Contact extends Component {
-  render() {
-    return (
-      <section
-        id="contact"
-        className="min-h-screen flex items-center justify-center py-20"
-      >
-        <div className="px-4 w-150">
-          <h2 className="text-3xl font-bold mb-8 text-center">
-            Please Reach Out!
-          </h2>
-          <form className="space-y-6">
-            <div className="relative">
-              <input
-                type="text"
-                id="name"
-                name="name"
-                required
-                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                placeholder="Name"
-              />
-            </div>
+export default function Contact() {
+  const [result, setResult] = React.useState("");
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const apiURL = import.meta.env.VITE_API_URL;
 
-            <div className="relative">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                required
-                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                placeholder="example@gmail.com"
-              />
-            </div>
-            <div className="relative">
-              <textarea
-                id="message"
-                name="message"
-                rows={6}
-                required
-                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                placeholder="Type your message here 😊"
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white py-3 px-6 font medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
-            >
-              Send
-            </button>
-          </form>
-        </div>
-      </section>
-    );
-  }
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending Email....");
+    const formData = new FormData(event.target);
+    console.log(apiKey);
+    formData.append("access_key", apiKey);
+
+    const response = await fetch(apiURL, {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Email Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <section
+      id="contact"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <div className="px-4 w-150">
+        <h2 className="text-3xl font-bold mb-8 text-center">
+          Please Reach Out!
+        </h2>
+        <h4 className="text-sm mb-2 text-center">
+          Reach out to dylanhunter34@gmail.com if there are any issues with the
+          form!
+        </h4>
+        <form className="space-y-6" onSubmit={onSubmit}>
+          <div className="relative">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+              placeholder="Name"
+            />
+          </div>
+
+          <div className="relative">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+              placeholder="example@gmail.com"
+            />
+          </div>
+          <div className="relative">
+            <textarea
+              id="message"
+              name="message"
+              rows={6}
+              required
+              className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+              placeholder="Type your message here 😊"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-3 px-6 font medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+          >
+            Send
+          </button>
+        </form>
+        <span className="w-full text-white font medium relative">{result}</span>
+      </div>
+    </section>
+  );
 }
-
-export default Contact;
