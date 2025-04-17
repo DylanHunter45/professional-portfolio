81da6e7f413b3d9443ed802f5f7c2d572207a96b
Dylan Hunter <dylanhunter34@gmail.com>
2025-04-17 12:29:47 -0400

    fix: update Achievements section with new titles and descriptions, add PDF icons for resumes

--- src/components/sections/Achievements.jsx
+++ src/components/sections/Achievements.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { FaFilePdf } from "react-icons/fa6";
 
 export class Achievements extends Component {
   render() {
@@ -12,23 +13,44 @@ export class Achievements extends Component {
             My Achievements
           </h2>
           <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246, 0.2)] transition-all">
-            <p className="text-gray-300 mb-6">Achievements section title</p>
-
+            <p className="text-gray-300 text-xl">
+              <strong>Work Achievements</strong>
+            </p>
             <div className="grid grid-cols-1 md:grid-cols-1 gap-6 mt-8">
               <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246, 0.2)] transition-all w-150">
-                <h3 className="text-xl font-bold mb-4"> Achievement 1 </h3>
+                <h3 className="text-xl font-bold mb-4">
+                  Management Sector Moving Markers Award 2024
+                </h3>
                 <ul className="list-disc list-inside text-gray-300 space-y-2">
-                  <li>point 1</li>
-                  <li>point 2</li>
-                  <li>point 3</li>
+                  <li>
+                    Prize granted by mangement to recognize employees who
+                    exeeded project expectations and milestones.
+                  </li>
+                  <a
+                    href="professional-portfolio/Dylan Hunter Resume 2024 old.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex justify-right text-red-500"
+                  >
+                    <FaFilePdf size={26} />
+                  </a>
                 </ul>
               </div>
+              <p className="text-gray-300 text-xl">
+                <strong>Schools Achievements</strong>
+              </p>
               <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246, 0.2)] transition-all">
-                <h3 className="text-xl font-bold mb-4"> Achievement 1 </h3>
+                <h3 className="text-xl font-bold mb-4">Dean's Honours List</h3>
                 <ul className="list-disc list-inside text-gray-300 space-y-2">
-                  <li>point 1</li>
-                  <li>point 2</li>
-                  <li>point 3</li>
+                  <li>Achieved an average GPA of 3.8/4.0 during my studies.</li>
+                  <a
+                    href="professional-portfolio/Dylan Hunter Resume 2024 old.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex justify-right text-red-500"
+                  >
+                    <FaFilePdf size={26} />
+                  </a>
                 </ul>
               </div>
             </div>
