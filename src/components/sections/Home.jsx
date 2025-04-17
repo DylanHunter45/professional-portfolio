4374fb83031d2c274af6d40039f1fdfdbb570fc9
Dylan Hunter <dylanhunter34@gmail.com>
2025-04-17 16:13:37 -0400

    fix: update resume link to point to the correct PDF file

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -52,7 +52,7 @@ export class Home extends Component {
               <FaGithub size={28} />
             </a>
             <a
-              href="professional-portfolio/Dylan Hunter Resume 2024 old.pdf"
+              href="professional-portfolio/Resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="text-red-500"
